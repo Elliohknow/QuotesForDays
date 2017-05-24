ff7d2251b88598c2e605e1ba2ca4de8301644ffc
Elliott Jones <EllyottJones@hotmail.com>
2017-05-24 18:05:01 -0400

    fixed a few issues

--- main.js
+++ main.js
@@ -1,1 +1,47 @@
 // JavaScript source code
+var quote = "";
+var author = "";
+function getNewQuote() {
+    $.ajax({
+        url: "http://api.forismatic.com/api/1.0/",
+        jsonp: "jsonp",
+        dataType: "jsonp",
+        data: {
+            method: "getQuote",
+            lang: "en",
+            format: "jsonp"
+        },
+        success: function (response) {
+            quote = response.quoteText;
+            author = response.quoteAuthor;
+
+            $("#qText").text(quote);
+            
+
+            if (author) {
+                $("#authorName").text("- " + author);
+                console.log(author);
+            } else {
+                $("#authorName").text(
+                    "- Your Dad. Seriously, he used to go around saying this all the time."
+                );
+            }
+        }
+    });
+}
+
+$(document).ready(function () {
+    getNewQuote();
+
+    $("#newQuote").on("click", function (event) {
+        event.preventDefault();
+        getNewQuote();
+    });
+
+    $("#tweet-it").on("click", function (event) {
+        event.preventDefault();
+        window.open(
+            "https://twitter.com/intent/tweet?text=" + encodeURIComponent(quote + "-- " + author)
+        );
+    });
+});
